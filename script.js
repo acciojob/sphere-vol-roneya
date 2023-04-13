5073bff66eb204c68eaa7c7e59ef8e17ade05cc5
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-04-13 22:13:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-function volume_sphere() {
-    //Write your code here
-  // Get the input element
+
 var myInput = document.getElementById("volume");
 var r = document.getElementById("radius");
-	
+
+function volume_sphere() {
+
+event.preventDefault();
 myInput.value = 3.14*(4/3)*r.value*r.value*r.value;
 
 } 
